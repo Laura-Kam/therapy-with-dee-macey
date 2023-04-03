f3fa6005891de2ad4f09130d0e36cd58031def87
Laura Kambanis <kambanislaura@gmail.com>
2023-04-03 18:47:03 +0100

    navbar dropdown menu fixed

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -1,199 +1,98 @@
 import Link from "next/link";
 import Image from "next/image";
 import rttlogo from "../../public/rttroundedlogo.png";
+import { useState } from "react";
 
 const Navbar = () => {
-  return (
-    // <nav>
-    //   <div className="navbar bg-base-100 navbar-expand-lg">
-    //     {" "}
-    //     <div className="navbar-start navbar-expand-lg">
-    //       <div className="dropdown">
-    //         <label tabIndex={0} className="btn btn-ghost lg:hidden">
-    //           <svg
-    //             xmlns="http://www.w3.org/2000/svg"
-    //             className="h-5 w-5"
-    //             fill="none"
-    //             viewBox="0 0 24 24"
-    //             stroke="currentColor"
-    //           >
-    //             <path
-    //               strokeLinecap="round"
-    //               strokeLinejoin="round"
-    //               strokeWidth="2"
-    //               d="M4 6h16M4 12h8m-8 6h16"
-    //             />
-    //           </svg>
-    //         </label>
-    //         <ul
-    //           tabIndex={0}
-    //           className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box"
-    //         >
-    //           <li>
-    //             <Link href="/">Home</Link>
-    //           </li>
-    //           <li>
-    //             <Link href="/whatisrtt">What is RTT?</Link>
-    //           </li>
-    //           <li>
-    //             <Link href="/treatment">Treatment</Link>
-    //           </li>
-    //           <li>
-    //             <Link href="/faq">FAQs</Link>
-    //           </li>
-    //           <li>
-    //             <Link href="/about">About Me</Link>
-    //           </li>
-    //         </ul>
-    //       </div>
-    //       <a className="btn btn-ghost normal-case text-xl flex items-center">
-    //         <Image
-    //           src="/rttroundedlogo.png"
-    //           alt="My Image"
-    //           width={170}
-    //           height={170}
-    //           layout="fixed"
-    //           className="w-12 h-12 object-contain"
-    //         />
-    //         <span className="ml-2 inline-block">Dee Macey</span>
-    //       </a>
-    //     </div>
-    //     <div className="navbar-center hidden lg:flex">
-    //       <ul className="menu menu-horizontal px-1">
-    //         <li>
-    //           <Link href="/">Home</Link>
-    //         </li>
-    //         <li>
-    //           <Link href="/whatisrtt">What is RTT?</Link>
-    //         </li>
-    //         <li>
-    //           <Link href="/treatment">Treatment</Link>
-    //         </li>
-    //         <li>
-    //           <Link href="/faq">FAQs</Link>
-    //         </li>
-    //         <li>
-    //           <Link href="/about">About Me</Link>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //     <div className="navbar-end hidden lg:block">
-    //       <a className="btn">CONTACT ME</a>
-    //     </div>
-    //   </div>
+  const [isOpen, setIsOpen] = useState(false);
 
-    //   {/* Mobile Responsive Navbar */}
-    //   <div className="lg:hidden">
-    //     <ul className="menu menu-compact mt-3 p-2 shadow bg-base-100 rounded-box">
-    //       <li>
-    //         <Link href="/">Home</Link>
-    //       </li>
-    //       <li>
-    //         <Link href="/whatisrtt">What is RTT?</Link>
-    //       </li>
-    //       <li>
-    //         <Link href="/treatment">Treatment</Link>
-    //       </li>
-    //       <li>
-    //         <Link href="/faq">FAQs</Link>
-    //       </li>
-    //       <li>
-    //         <Link href="/about">About Me</Link>
-    //       </li>
-    //     </ul>
-    //   </div>
-    // </nav>
-    <div className="navbar bg-base-100" style={{ width: "100%" }}>
-      <div className="navbar-start">
-        <div className="dropdown">
-          <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
+  const toggleDropdown = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <nav>
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <label
+              tabIndex={0}
+              className="btn btn-ghost lg:hidden"
+              onClick={toggleDropdown}
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />
-            </svg>
-          </label>
-          <ul
-            tabIndex={0}
-            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
-          >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </label>
+            <ul
+              tabIndex={0}
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/whatisrtt">What is RTT?</Link>
+              </li>
+              <li>
+                <Link href="/treatment">Treatment</Link>
+              </li>
+              <li>
+                <Link href="/faq">FAQs</Link>
+              </li>
+              <li>
+                <Link href="/about">About Me</Link>
+              </li>
+            </ul>
+          </div>
+          <a className="btn btn-ghost normal-case text-xl flex items-center">
+            <Image
+              src="/rttroundedlogo.png"
+              alt="My Image"
+              width={170}
+              height={170}
+              layout="fixed"
+              className="w-12 h-12 object-contain"
+            />
+            <span className="ml-2 inline-block">Dee Macey</span>
+          </a>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
             <li>
-              <a>Item 1</a>
-            </li>
-            <li tabIndex={0}>
-              <a className="justify-between">
-                Parent
-                <svg
-                  className="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                </svg>
-              </a>
-              <ul className="p-2">
-                <li>
-                  <a>Submenu 1</a>
-                </li>
-                <li>
-                  <a>Submenu 2</a>
-                </li>
-              </ul>
+              <Link href="/">Home</Link>
             </li>
             <li>
-              <a>Item 3</a>
+              <Link href="/whatisrtt">What is RTT?</Link>
+            </li>
+            <li>
+              <Link href="/treatment">Treatment</Link>
+            </li>
+            <li>
+              <Link href="/faq">FAQs</Link>
+            </li>
+            <li>
+              <Link href="/about">About Me</Link>
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <div className="navbar-end">
+          <Link href="/contact" className="btn">
+            CONTACT ME
+          </Link>
+        </div>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-            <a>Item 1</a>
-          </li>
-          <li tabIndex={0}>
-            <a>
-              Parent
-              <svg
-                className="fill-current"
-                xmlns="http://www.w3.org/2000/svg"
-                width="20"
-                height="20"
-                viewBox="0 0 24 24"
-              >
-                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-              </svg>
-            </a>
-            <ul className="p-2">
-              <li>
-                <a>Submenu 1</a>
-              </li>
-              <li>
-                <a>Submenu 2</a>
-              </li>
-            </ul>
-          </li>
-          <li>
-            <a>Item 3</a>
-          </li>
-        </ul>
-      </div>
-      <div className="navbar-end">
-        <a className="btn">Get started</a>
-      </div>
-    </div>
+    </nav>
   );
 };
 
