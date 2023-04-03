0aeefbe2a4bb1c5167ee45cd9a2ecad06e2cc60d
Laura Kambanis <kambanislaura@gmail.com>
2023-04-03 18:53:01 +0100

    edited navbar

--- src/comps/Navbar.js
+++ src/comps/Navbar.js
@@ -9,6 +9,7 @@ const Navbar = () => {
   const toggleDropdown = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <nav>
       <div className="navbar bg-base-100">
@@ -36,7 +37,9 @@ const Navbar = () => {
             </label>
             <ul
               tabIndex={0}
-              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              className={`menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 ${
+                isOpen ? "dropdown-open" : ""
+              }`}
             >
               <li>
                 <Link href="/">Home</Link>
