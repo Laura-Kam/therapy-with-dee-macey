c00e1e7926ee26d26df5e4a57a4e184fbbcd17d7
Laura Kambanis <kambanislaura@gmail.com>
2023-04-08 18:06:54 +0100

    changing padding on info boxes

--- src/comps/HomepageComponents/InfoBoxesHomePage.js
+++ src/comps/HomepageComponents/InfoBoxesHomePage.js
@@ -1,3 +1,118 @@
+// import Link from "next/link";
+// import Image from "next/image";
+
+// const InfoBoxesHomepage = () => {
+//   return (
+//     <div className="outer-div bg-cyan-100">
+//       <div className="">
+//         <p className="text-xl mb-2 font-bold text-center p-10">
+//           What are the benefits of RTT for you? <br />
+//           <br />
+//           <span className="pb-5">Issues treated</span>
+//         </p>
+//       </div>
+//       <div className="">
+//         <div className="flex flex-wrap justify-center">
+//           <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+//             <figure className="px-3 pt-10">
+//               <img
+//                 src="https://images.unsplash.com/photo-1527106670449-cf7c7e31af4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+//                 alt="Shoes"
+//                 className="h-30 w-80 rounded-xl"
+//               />
+//             </figure>
+//             <div className="card-body items-center text-center">
+//               <h2 className="card-title">Shoes!</h2>
+//               <p className="text-sm">
+//                 If a dog chews shoes whose shoes does he choose?
+//               </p>
+//               <div className="card-actions">
+//                 <button className="btn btn-primary text-sm">Buy Now</button>
+//               </div>
+//             </div>
+//           </div>
+
+//           <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+//             <figure className="px-10 pt-10">
+//               <img
+//                 src="https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+//                 alt="Shoes"
+//                 className="h-30 w-80 rounded-xl"
+//               />
+//             </figure>
+//             <div className="card-body items-center text-center">
+//               <h2 className="card-title">Shoes!</h2>
+//               <p className="text-sm">
+//                 If a dog chews shoes whose shoes does he choose?
+//               </p>
+//               <div classNames="card-actions">
+//                 <button className="btn btn-primary text-sm">Buy Now</button>
+//               </div>
+//             </div>
+//           </div>
+
+//           <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+//             <figure className="px-10 pt-10">
+//               <img
+//                 src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+//                 alt="Shoes"
+//                 className="h-30 w-80 rounded-xl"
+//               />
+//             </figure>
+//             <div className="card-body items-center text-center">
+//               <h2 className="card-title">Shoes!</h2>
+//               <p className="text-sm">
+//                 If a dog chews shoes whose shoes does he choose?
+//               </p>
+//               <div className="card-actions">
+//                 <button className="btn btn-primary text-sm">Buy Now</button>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+//             <figure className="px-3 pt-10">
+//               <img
+//                 src="https://images.unsplash.com/photo-1527106670449-cf7c7e31af4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+//                 alt="Shoes"
+//                 className="h-30 w-80 rounded-xl"
+//               />
+//             </figure>
+//             <div className="card-body items-center text-center">
+//               <h2 className="card-title">Shoes!</h2>
+//               <p className="text-sm">
+//                 If a dog chews shoes whose shoes does he choose?
+//               </p>
+//               <div className="card-actions">
+//                 <button className="btn btn-primary text-sm">Buy Now</button>
+//               </div>
+//             </div>
+//           </div>
+//           <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+//             <figure className="px-3 pt-10">
+//               <img
+//                 src="https://images.unsplash.com/photo-1527106670449-cf7c7e31af4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+//                 alt="Shoes"
+//                 className="h-30 w-80 rounded-xl"
+//               />
+//             </figure>
+//             <div className="card-body items-center text-center">
+//               <h2 className="card-title">Shoes!</h2>
+//               <p className="text-sm">
+//                 If a dog chews shoes whose shoes does he choose?
+//               </p>
+//               <div className="card-actions">
+//                 <button className="btn btn-primary text-sm">Buy Now</button>
+//               </div>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default InfoBoxesHomepage;
+
 import Link from "next/link";
 import Image from "next/image";
 
@@ -5,20 +120,20 @@ const InfoBoxesHomepage = () => {
   return (
     <div className="outer-div bg-cyan-100">
       <div className="">
-        <p className="text-xl mb-12 font-bold text-center p-10">
+        <p className="text-xl mb-2 font-bold text-center p-10">
           What are the benefits of RTT for you? <br />
           <br />
-          <span className="not-bold">Issues treated</span>
+          <span className="pb-5">Issues treated</span>
         </p>
       </div>
       <div className="">
         <div className="flex flex-wrap justify-center">
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
-            <figure className="px-3 pt-10">
+          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+            <figure className="px-3 pt-5">
               <img
                 src="https://images.unsplash.com/photo-1527106670449-cf7c7e31af4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 alt="Shoes"
-                className="h-30 w-80 rounded-xl"
+                className="h-20 w-64 rounded-xl"
               />
             </figure>
             <div className="card-body items-center text-center">
@@ -32,12 +147,12 @@ const InfoBoxesHomepage = () => {
             </div>
           </div>
 
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
-            <figure className="px-10 pt-10">
+          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+            <figure className="px-10 pt-5">
               <img
                 src="https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 alt="Shoes"
-                className="h-30 w-80 rounded-xl"
+                className="h-20 w-64 rounded-xl"
               />
             </figure>
             <div className="card-body items-center text-center">
@@ -45,18 +160,18 @@ const InfoBoxesHomepage = () => {
               <p className="text-sm">
                 If a dog chews shoes whose shoes does he choose?
               </p>
-              <div classNames="card-actions">
+              <div className="card-actions">
                 <button className="btn btn-primary text-sm">Buy Now</button>
               </div>
             </div>
           </div>
 
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
-            <figure className="px-10 pt-10">
+          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+            <figure className="px-10 pt-5">
               <img
-                src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                src="https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 alt="Shoes"
-                className="h-30 w-80 rounded-xl"
+                className="h-20 w-64 rounded-xl"
               />
             </figure>
             <div className="card-body items-center text-center">
@@ -69,12 +184,12 @@ const InfoBoxesHomepage = () => {
               </div>
             </div>
           </div>
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
-            <figure className="px-3 pt-10">
+          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+            <figure className="px-10 pt-5">
               <img
-                src="https://images.unsplash.com/photo-1527106670449-cf7c7e31af4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                src="https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 alt="Shoes"
-                className="h-30 w-80 rounded-xl"
+                className="h-20 w-64 rounded-xl"
               />
             </figure>
             <div className="card-body items-center text-center">
@@ -87,12 +202,12 @@ const InfoBoxesHomepage = () => {
               </div>
             </div>
           </div>
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
-            <figure className="px-3 pt-10">
+          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-sky-100">
+            <figure className="px-10 pt-5">
               <img
-                src="https://images.unsplash.com/photo-1527106670449-cf7c7e31af4e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                src="https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
                 alt="Shoes"
-                className="h-30 w-80 rounded-xl"
+                className="h-20 w-64 rounded-xl"
               />
             </figure>
             <div className="card-body items-center text-center">
