b946eeaf267d8ae603fe2516d85d7a1a530bc288
Laura Kambanis <kambanislaura@gmail.com>
2023-04-09 14:21:25 +0100

    added symbols and design to 3 divs for issues treated

--- src/comps/HomepageComponents/IssuesTreatedHomepage.js
+++ src/comps/HomepageComponents/IssuesTreatedHomepage.js
@@ -1,27 +1,29 @@
 import Link from "next/link";
 import Image from "next/image";
 import { TiTickOutline } from "react-icons/ti";
+import { TbBulb } from "react-icons/tb";
 
 const IssuesTreatedHomepage = () => {
   return (
     <div className="bg-white p-4">
-      <h1 className="text-xl font-bold mb-2 text-center">
+      <h1 className="text-xl font-bold mb-2 text-center mt-6">
         Can RTT work for me?
       </h1>
-      <p className="mb-4 text-center ">
+      <p className="mb-4 text-center m-8 ml-12 mr-10 ">
         If your heart’s desire is for change, then RTT can definitely work for
         you. This will be dependent on you listening to your bespoke audio for
         at least 21 days and for you to be willing to enter into a ‘hypnosis
         contract’. This is your willingness to go into hypnosis with your
         therapist.
       </p>
-      <h1 className="text-xl font-bold mb-2 text-center text-teal-950">
+      <h1 className="text-xl font-bold mb-10 mt-8 text-center text-teal-950">
         What can RTT be used for?
       </h1>
-      <div className="flex flex-col sm:flex-row gap-4">
-        <div className="bg-gray-100 p-4 flex-1 mx-20 sm:w-1/3">
-          <h2 className="text-lg font-bold mb-2 text-center">List 1</h2>
-          <ul className="list-disc">
+      <div className="flex flex-col sm:flex-row gap-4 items-center">
+        <div className="bg-gray-100 p-4 flex-1 mx-20 sm:w-1/3 md:2/3 relative">
+          <TbBulb className="mb-5 text-4xl absolute top-4 bottom-3 left-1/2 transform -translate-x-1/2" />
+
+          <ul className="list-none mt-12">
             <li className="mb-2">
               <TiTickOutline className="inline-block mr-2" />
               Anxiety
@@ -60,7 +62,7 @@ const IssuesTreatedHomepage = () => {
             </li>
           </ul>
         </div>
-        <div className="bg-gray-100 p-4 flex-1 mx-20 sm:w-1/3">
+        <div className="bg-gray-100 p-4 flex-1 mx-20 sm:w-1/3 md:2/3">
           <h2 className="text-lg font-bold mb-2 text-center">List 2</h2>
           <ul className="list-none">
             <li className="mb-2">
@@ -101,7 +103,7 @@ const IssuesTreatedHomepage = () => {
             </li>
           </ul>
         </div>
-        <div className="bg-gray-100 p-4 flex-1 mx-20 sm:w-1/3">
+        <div className="bg-gray-100 p-4 flex-1 mx-20 sm:w-1/3 md:2/3">
           <h2 className="text-lg font-bold mb-2 text-center">List 2</h2>
           <ul className="list-none">
             <li className="mb-2">
