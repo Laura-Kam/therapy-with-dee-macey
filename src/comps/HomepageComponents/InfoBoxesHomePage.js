69c7eb65af29a1a59f7fddc1623b8d0ea3df557d
Laura Kambanis <kambanislaura@gmail.com>
2023-04-09 19:37:05 +0100

    attempt to sort out mobile screen width issue

--- src/comps/HomepageComponents/InfoBoxesHomePage.js
+++ src/comps/HomepageComponents/InfoBoxesHomePage.js
@@ -18,116 +18,118 @@ const InfoBoxesHomepage = () => {
       </div>
       <div className="">
         <div className="flex flex-wrap justify-center p-5">
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
-            <figure className="px-10 pt-5">
-              <img
-                src="https://images.unsplash.com/photo-1559724087-a45f6a7a35d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=852&q=80"
-                alt="Shoes"
-                className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Weight</h2>
-              <p className="text-sm">
-                What weight would you like to be to feel healthy?
-              </p>
+         
+            <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
+              <figure className="px-10 pt-5">
+                <img
+                  src="https://images.unsplash.com/photo-1559724087-a45f6a7a35d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=852&q=80"
+                  alt="Shoes"
+                  className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Weight</h2>
+                <p className="text-sm">
+                  What weight would you like to be to feel healthy?
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
-            <figure className="px-10 pt-5">
-              <img
-                src="https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                alt="Shoes"
-                className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Motivation</h2>
-              <p className="text-sm">
-                I.e. to be more motivated to study, to get that job you have
-                always wanted or to go to the gym or a dance class
-              </p>
+            <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
+              <figure className="px-10 pt-5">
+                <img
+                  src="https://images.unsplash.com/photo-1613312328068-c9b6b76c9e8a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  alt="Shoes"
+                  className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Motivation</h2>
+                <p className="text-sm">
+                  I.e. to be more motivated to study, to get that job you have
+                  always wanted or to go to the gym or a dance class
+                </p>
+              </div>
             </div>
-          </div>
 
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
-            <figure className="px-10 pt-5">
-              <img
-                src="https://images.unsplash.com/photo-1465658428880-830aab5e4e30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                alt="Shoes"
-                className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">An addiction</h2>
-              <p className="text-sm">
-                An addiction. Would you like to stop smoking once and for all?
-              </p>
+            <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
+              <figure className="px-10 pt-5">
+                <img
+                  src="https://images.unsplash.com/photo-1465658428880-830aab5e4e30?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  alt="Shoes"
+                  className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">An addiction</h2>
+                <p className="text-sm">
+                  An addiction. Would you like to stop smoking once and for all?
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
-            <figure className="px-10 pt-5">
-              <img
-                src="https://images.unsplash.com/photo-1499171138085-a60c4e752ff7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
-                alt="Shoes"
-                className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Guilt</h2>
-              <p className="text-sm">
-                Finally put down that heavy burden you have been carrying for so
-                long it feels like it’s a part of you.
-              </p>
+            <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
+              <figure className="px-10 pt-5">
+                <img
+                  src="https://images.unsplash.com/photo-1499171138085-a60c4e752ff7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
+                  alt="Shoes"
+                  className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Guilt</h2>
+                <p className="text-sm">
+                  Finally put down that heavy burden you have been carrying for
+                  so long it feels like it’s a part of you.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
-            <figure className="px-10 pt-5">
-              <img
-                src="https://images.unsplash.com/photo-1618616191524-a9721186cbe4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
-                alt="Shoes"
-                className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Anxiety</h2>
-              <p className="text-sm">
-                To have the opportunity to understand why you feel anxious. What
-                is driving it?
-              </p>
+            <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
+              <figure className="px-10 pt-5">
+                <img
+                  src="https://images.unsplash.com/photo-1618616191524-a9721186cbe4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869&q=80"
+                  alt="Shoes"
+                  className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Anxiety</h2>
+                <p className="text-sm">
+                  To have the opportunity to understand why you feel anxious.
+                  What is driving it?
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
-            <figure className="px-10 pt-5">
-              <img
-                src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                alt="Shoes"
-                className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Money Blocks</h2>
-              <p className="text-sm">
-                dig deep to discover why you believe money is not for you and
-                how this has influenced your life
-              </p>
+            <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
+              <figure className="px-10 pt-5">
+                <img
+                  src="https://images.unsplash.com/photo-1563013544-824ae1b704d3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  alt="Shoes"
+                  className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Money Blocks</h2>
+                <p className="text-sm">
+                  dig deep to discover why you believe money is not for you and
+                  how this has influenced your life
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
-            <figure className="px-10 pt-5">
-              <img
-                src="https://images.unsplash.com/photo-1604031187853-ceda1f4a446c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
-                alt="Shoes"
-                className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
-              />
-            </figure>
-            <div className="card-body items-center text-center">
-              <h2 className="card-title">Negative Repetitive Thoughts</h2>
-              <p className="text-sm">
-                ‘Relationships don’t work for me and it will always be this
-                way’.
-              </p>
+            <div className="card w-full sm:w-1/2 md:w-1/2 lg:w-1/5 xl:w-1/5 bg-base-100 shadow-xl mx-2 my-2 bg-slate-50">
+              <figure className="px-10 pt-5">
+                <img
+                  src="https://images.unsplash.com/photo-1604031187853-ceda1f4a446c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+                  alt="Shoes"
+                  className="rounded-xl h-45 sm:h-45 md:h-50 lg:h-50 xl:h-50 w-auto sm:w-55 md:w-60 lg:w-65 xl:w-60"
+                />
+              </figure>
+              <div className="card-body items-center text-center">
+                <h2 className="card-title">Negative Repetitive Thoughts</h2>
+                <p className="text-sm">
+                  ‘Relationships don’t work for me and it will always be this
+                  way’.
+                </p>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/treatment/index.js
+++ src/pages/treatment/index.js
@@ -1,77 +1,10 @@
 import Layout from "../../comps/Layout";
 
 const Treatment = () => {
-  return (
-    <Layout>
-      <div>
-        <h1>Rapid Transformational Therapy Sessions</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
-          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
-          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
-          sequi!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
-          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
-          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
-          sequi!
-        </p>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa porro
-          reiciendis ea quibusdam nemo nobis hic iure quos odio corporis,
-          laudantium animi laborum voluptatem quia, veniam fuga aperiam officia
-          sequi!
-        </p>
-      </div>
-    </Layout>
-  );
+  return <Layout></Layout>;
 };
 
 export default Treatment;
 
 import React from "react";
 // import 'daisyui/dist/daisyui.css';
-
-<div className="grid grid-cols-4 gap-4">
-  <div className="card">
-    <div className="card-body">
-      <h2 className="card-title">Card 1</h2>
-      <p>Some text here.</p>
-      <div className="card-actions">
-        <button className="btn btn-primary">Action 1</button>
-        <button className="btn btn-secondary">Action 2</button>
-      </div>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-body">
-      <h2 className="card-title">Card 2</h2>
-      <p>Some text here.</p>
-      <div className="card-actions">
-        <button className="btn btn-primary">Action 1</button>
-        <button className="btn btn-secondary">Action 2</button>
-      </div>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-body">
-      <h2 className="card-title">Card 3</h2>
-      <p>Some text here.</p>
-      <div className="card-actions">
-        <button className="btn btn-primary">Action 1</button>
-        <button className="btn btn-secondary">Action 2</button>
-      </div>
-    </div>
-  </div>
-  <div className="card">
-    <div className="card-body">
-      <h2 className="card-title">Card 4</h2>
-      <p>Some text here.</p>
-      <div className="card-actions">
-        <button className="btn btn-primary">Action 1</button>
-        <button className="btn btn-secondary">Action 2</button>
-      </div>
-    </div>
-  </div>
-</div>;
