8e2bd3bbcaddf39cdcc555724f30989d63de29dd
Jordi <jordi.lanau@yahoo.com>
2023-04-13 17:30:08 +0100

    Remove image overlap from fotter

--- src/comps/Footer.js
+++ src/comps/Footer.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 const Footer = () => {
   return (
     <footer
-      className="footer footer-center p-7 flex-wrap"
+      className="footer footer-center p-7 flex flex-col flex-wrap"
       style={{
         backgroundColor: "#F5F5F5",
         color: "black",
@@ -12,7 +12,7 @@ const Footer = () => {
         fontSize: "16px",
       }}
     >
-      <div className="footerDiv grid grid-cols-2 md:grid-cols-5 lg:grid-cols-7 gap-3 items-center text-center mb-5 flex-wrap">
+      <div className="grid grid-cols-2 md:grid-cols-5 lg:grid-cols-7 gap-3 text-center">
         <Link href="/" className="link link-hover">
           Home
         </Link>
@@ -36,7 +36,7 @@ const Footer = () => {
         </Link>
       </div>
 
-      <div className="flex justify-center flex-wrap">
+      <div>
         <Image
           src="/rttroundedlogo.png"
           alt="My Image"
