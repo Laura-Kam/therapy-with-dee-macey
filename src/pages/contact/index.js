5c55e97f894d2558a513c8cff4a5bc5abb013802
Jordi <jordi.lanau@yahoo.com>
2023-05-07 10:59:46 +0100

    Add nodemailer

--- src/pages/contact/index.js
+++ src/pages/contact/index.js
@@ -1,14 +1,13 @@
-import { useState } from "react";
-import Layout from "../../comps/Layout";
-import { MessageSchema } from "../../utils/MessageSchema";
-import Image from "next/image";
+import { useState } from 'react';
+import Layout from '../../comps/Layout';
+import { MessageSchema } from '../../utils/MessageSchema';
 
 const Contact = () => {
   const initialState = {
-    name: "",
-    email: "",
-    phoneNumber: "",
-    message: "",
+    name: '',
+    email: '',
+    phoneNumber: '',
+    message: '',
   };
 
   const [formState, setFormState] = useState(initialState);
@@ -24,19 +23,19 @@ const Contact = () => {
   }
 
   async function sendMessage(message) {
-    const response = await fetch("/api/contact", {
-      method: "POST",
+    const response = await fetch('/api/contact', {
+      method: 'POST',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(message),
     });
 
     const data = await response.json();
     if (response.ok) {
-      console.log("success:", data);
+      console.log('success:', data);
     } else {
-      console.log("error:", data);
+      console.log('error:', data);
     }
   }
 
@@ -47,12 +46,10 @@ const Contact = () => {
 
     switch (formData.success) {
       case true:
-        console.log("valid data");
         setErrors({});
         sendMessage(formData.data);
         break;
       case false:
-        console.log("invalid data");
         const formattedErrors = formData.error.format();
         setErrors({
           name: formattedErrors.name?._errors[0] || undefined,
@@ -66,62 +63,60 @@ const Contact = () => {
 
   return (
     <Layout>
-      <div className="text-center m-0 p-20 bg-sky-100">
-        <div className="upperTextContactForm">
-          <h1 className="font-bold text-lg">
+      <div className='text-center m-0 p-20 bg-sky-100'>
+        <div className='upperTextContactForm'>
+          <h1 className='font-bold text-lg'>
             Contact Me
             <br />
-            <span className="font-normal text-lg">
-              Your first consultation is free
-            </span>
+            <span className='font-normal text-lg'>Your first consultation is free</span>
           </h1>
         </div>
         {/* formdiv */}
-        <div className="w-full sm:w-1/3 md:w-1/2 lg:w-1/3 mx-auto bg-sky-100 form-control input-bordered mt-10 h-1/5">
+        <div className='w-full sm:w-1/3 md:w-1/2 lg:w-1/3 mx-auto bg-sky-100 form-control input-bordered mt-10 h-1/5'>
           <form onSubmit={handleFormSubmit}>
-            <label className="input-group input-group-vertical">
-              <span className="w-auto m-5 h-50">First Name and Surname</span>
+            <label className='input-group input-group-vertical'>
+              <span className='w-auto m-5 h-50'>First Name and Surname</span>
               <input
-                type="text"
-                className="input input-bordered"
-                name="name"
-                id="name"
+                type='text'
+                className='input input-bordered'
+                name='name'
+                id='name'
                 value={formState.name}
                 onChange={handleFormChange}
               />
               {!!errors.name && <p>{errors.name}</p>}
             </label>
-            <label className="input-group input-group-vertical">
-              <span className="w-auto m-5 h-50">Email Address</span>
+            <label className='input-group input-group-vertical'>
+              <span className='w-auto m-5 h-50'>Email Address</span>
               <input
-                type="text"
-                className="input input-bordered"
-                name="email"
-                id="email"
+                type='text'
+                className='input input-bordered'
+                name='email'
+                id='email'
                 value={formState.email}
                 onChange={handleFormChange}
               />
               {!!errors.email && <p>{errors.email}</p>}
             </label>
-            <label className="input-group input-group-vertical">
-              <span className="w-auto m-5 h-50">Mobile Phone Number</span>
+            <label className='input-group input-group-vertical'>
+              <span className='w-auto m-5 h-50'>Mobile Phone Number</span>
               <input
-                type="text"
-                className="input input-bordered"
-                name="phoneNumber"
-                id="phoneNumber"
+                type='text'
+                className='input input-bordered'
+                name='phoneNumber'
+                id='phoneNumber'
                 value={formState.phoneNumber}
                 onChange={handleFormChange}
               />
               {!!errors.phoneNumber && <p>{errors.phoneNumber}</p>}
             </label>
-            <label className="input-group input-group-vertical">
-              <span className="w-auto m-5 h-3/5">Type Your Message Below</span>
+            <label className='input-group input-group-vertical'>
+              <span className='w-auto m-5 h-3/5'>Type Your Message Below</span>
               <textarea
-                type="text"
-                className="input input-bordered h-60 resize-y"
-                name="message"
-                id="message"
+                type='text'
+                className='input input-bordered h-60 resize-y'
+                name='message'
+                id='message'
                 value={formState.message}
                 onChange={handleFormChange}
               />
@@ -129,9 +124,8 @@ const Contact = () => {
             </label>
 
             <button
-              className="bg-amber-50 border-2 border-black rounded-full py-2 px-4 inline-block text-black hover:text-black  hover:bg-amber-200 focus:outline-none focus:shadow-outline m-5"
-              type="submit"
-            >
+              className='bg-amber-50 border-2 border-black rounded-full py-2 px-4 inline-block text-black hover:text-black  hover:bg-amber-200 focus:outline-none focus:shadow-outline m-5'
+              type='submit'>
               Submit
             </button>
           </form>
